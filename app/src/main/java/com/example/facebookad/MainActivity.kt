c5daa15e26f1bf46487aa2797548600015839bc4
alokranjanonline <alokranjan999@gmail.com>
2024-01-07 11:49:11 +0530

    07-Jan-2024
    Facebook Banner Ad Working

--- app/src/main/java/com/example/facebookad/MainActivity.kt
+++ app/src/main/java/com/example/facebookad/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.facebookad
 
 
 import android.os.Bundle
-import android.view.View
 import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
-import com.google.android.gms.ads.AdSize
-import com.google.android.gms.ads.AdView
+import com.facebook.ads.*
+import com.facebook.ads.AdSize
+import com.facebook.ads.AdView
+
 
 
 class MainActivity : ComponentActivity() {
@@ -14,10 +15,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        adView = AdView(this, "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID", AdSize.BANNER_50)
+        adView = AdView(this, "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID", AdSize.BANNER_HEIGHT_50)
         //val adContainer = findViewById<View>(R.id.banner_container) as LinearLayout
         val adContainer = findViewById<LinearLayout>(R.id.banner_container)
         adContainer.addView(adView)
         adView.loadAd()
+    }
+    override fun onDestroy() {
+        if (adView != null) {
+            adView.destroy()
+        }
+        super.onDestroy()
     }
 }
